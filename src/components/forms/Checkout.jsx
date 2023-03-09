761f8f5e77198c7decc8115c3625fcbcbc3dd19f
Anthony Paguaga <pagua210@gmail.com>
2023-03-09 00:02:21 -0600

    tailwind setup

--- src/components/forms/Checkout.jsx
+++ src/components/forms/Checkout.jsx
@@ -1,6 +1,74 @@
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <div className="mt-5 md:col-span-2 md:mt-0">
+      <form action="#" method="POST">
+        <div>
+          <div className="bg-white px-4 py-5 sm:p-6">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                  First name
+                </label>
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-4">
+                <label htmlFor="email-address" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <input
+                  type="text"
+                  name="email-address"
+                  id="email-address"
+                  autoComplete="email"
+                  className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+              <div className="col-span-6">
+                <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+                  Street address
+                </label>
+                <input
+                  type="text"
+                  name="street-address"
+                  id="street-address"
+                  autoComplete="street-address"
+                  className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
